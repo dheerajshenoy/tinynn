8c36cacb2ffa521f3c501ba22f9ea733fc8a08f3
Dheeraj Vittal Shenoy <dheerajshenoy22@gmail.com>
2026-01-28 01:09:55 +0100

    update

--- examples/and_or_xor.py
+++ examples/and_or_xor.py
@@ -3,20 +3,29 @@ import tinynn
 from tinynn.nn import NN
 from tinynn.layer import Dense, LeakyReLU, Sigmoid
 
-nn = NN()
-nn.layers = [Dense(2, 5), LeakyReLU(), Dense(5, 1), Sigmoid()]
-nn.loss_func = tinynn.loss.MSELoss()
-
-X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]], dtype=np.float32)
-y = np.array([[0], [1], [1], [1]], dtype=np.float32)
-
+# Hyperparameters
 epochs = 5000
+lr = 0.1
+loss_func = tinynn.loss.MSELoss()
 
+# Input and output data
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]], dtype=np.float32)
+y = np.array([[0], [0], [0], [1]], dtype=np.float32)  # AND logic gate
+
+# y = np.array([[0], [1], [1], [1]], dtype=np.float32) # OR logic gate
+# y = np.array([[0], [1], [1], [0]], dtype=np.float32) # XOR logic gate
+
+# Define the neural network
+nn = NN()
+nn.layers = [Dense(2, 10), LeakyReLU(), Dense(10, 1), Sigmoid()]
+nn.loss_func = loss_func
+
+# Training loop
 for i in range(epochs):
     y_pred = nn.forward(X)
     loss = nn.loss(y_pred, y)
     nn.backward(y, y_pred)
-    nn.step(learning_rate=0.1)
+    nn.step(learning_rate=lr)
 
     if i % 500 == 0:
         print("Epoch", i, "Loss:", loss.round(3))
